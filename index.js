4e3de1a422c6b602c8adebb3f2a93a7153688958
Sridharraj2023 <149178862+Sridharraj2023@users.noreply.github.com>
2024-04-27 12:56:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,86 @@
+
+let h1 = document.getElementById("h1-display"); 
+              
+
+                setTimeout(()=>{
+                     h1.innerHTML = "";
+                     h1.innerHTML = 10;
+                        setTimeout(()=>{
+                            h1.innerHTML = "";
+                             h1.innerHTML = 9;
+                            setTimeout(()=>{
+                                h1.innerHTML = "";
+                                h1.innerHTML = 8;                               
+                                setTimeout(()=>{
+                                    h1.innerHTML = "";
+                                    h1.innerHTML = 7;
+                                    setTimeout(()=>{
+                                        h1.innerHTML = "";
+                                        h1.innerHTML = 6;
+                                        setTimeout(()=>{
+                                            h1.innerHTML = "";
+                                            h1.innerHTML = 5;
+                                            setTimeout(()=>{
+                                                h1.innerHTML = "";
+                                                h1.innerHTML = 4;
+                                                setTimeout(()=>{
+                                                    h1.innerHTML = "";
+                                                    h1.innerHTML = 3;
+                                                    setTimeout(()=>{
+                                                        h1.innerHTML = "";
+                                                        h1.innerHTML = 2;
+                                                        setTimeout(()=>{
+                                                            h1.innerHTML = "";
+                                                            h1.innerHTML = 1;
+                                                            setTimeout(()=>{
+                                                                h1.innerHTML = "";
+                                                                h1.innerHTML = "Happy Independence Day !!";
+                                                               
+                                                            }, 1000)
+                                                        }, 1000)
+                                                    }, 1000)
+                                                }, 1000)
+                                            }, 1000)
+                                        }, 1000)
+                                    }, 1000)
+                                }, 1000)
+                    
+                            }, 1000)
+                        }, 1000)
+                    
+                    }, 1000)
+                    ;
+
+
+
+//Below are the practice code of the day. Please ignore that....*******************
+
+ // function display(value) {
+//     console.log(+value);
+// }
+
+// function add(a, b, display)
+
+// {
+//     display (a+b);
+// }
+
+// add(10, 20, display)
+
+// let values = [1,2,3,4,5]
+// function forEachOwn(array, callback){
+//     for (let i=0; i<array.length; i++)
+//     {
+//         callback(values[i])
+//     }        
+// }
+
+// forEachOwn(values, (e)=>{
+//     console.log("callback " +e);
+// }
+// )
+
+// values.forEach((e) => {
+//     console.log("ForEach "+e)
+    
+// });
